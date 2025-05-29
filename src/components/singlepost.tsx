859b2d176978d96fc85b8ba811fcd149a6f70990
dhpitt <david.h.pitt@gmail.com>
2025-05-29 14:09:39 -0700

    styled markdown prose properly

--- src/components/singlepost.tsx
+++ src/components/singlepost.tsx
@@ -2,7 +2,7 @@ import { Post } from '@/types/post'
 
 export default function SinglePost({ post }: { post: Post }) {
   return (
-    <article className="prose dark:prose-invert max-w-none">
+    <div className="max-w-none">
       <h1 className="text-2xl font-heading sm:text-4xl">
         {post.frontmatter.title}
       </h1>
@@ -10,13 +10,10 @@ export default function SinglePost({ post }: { post: Post }) {
       <p className="text-muted-foreground text-lg">
         {post.frontmatter.date}
       </p>
-      
-      <br>
-      </br>
 
-      <div className="font-base"
+      <div className="prose dark:prose-invert font-base"
         dangerouslySetInnerHTML={{ __html: post.contentHtml }} 
       />
-    </article>
+    </div>
   )
 }
--- src/data/posts/about.md
+++ src/data/posts/about.md
@@ -3,4 +3,11 @@ title: "About David"
 date: "2025-05-29"
 ---
 
-This is a post about David and his thoughts.
+# Hello World
+
+This is a **bold** test.
+
+- List item 1
+- List item 2
+
+[Link to Google](https://www.google.com)
