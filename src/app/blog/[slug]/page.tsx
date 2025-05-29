fe114107ef45be0b738dca02d7ccc64c3c38c3c6
dhpitt <david.h.pitt@gmail.com>
2025-05-29 12:21:31 -0700

    await posts properly

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,18 +3,17 @@ import SinglePost from '@/components/singlepost'
 import { notFound } from 'next/navigation'
 import { Post } from '@/types/post'
 
-type Params = {
-  slug: string
-}
+
+export const dynamic = 'force-static'
 
 export async function generateStaticParams() {
-  const posts = getAllPosts()
-  return posts.map((post) => ({ slug: post.slug }))
+  const posts = await getAllPosts()
+  return posts.map(post => ({ slug: post.slug }))
 }
 
-export default async function BlogPostPage({ params }: { params: Params }) {
-  const post = await getPostBySlug(params.slug)
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const awaitedParams = await params
+  const post = await getPostBySlug(awaitedParams.slug)
   if (!post) return notFound()
-
   return <SinglePost post={post} />
 }
