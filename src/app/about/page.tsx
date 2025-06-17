8761dc6e7c543ee42f9042e3bc89d572607efb35
dhpitt <david.h.pitt@gmail.com>
2025-06-17 08:09:47 -0400

    cleanup about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,17 +3,7 @@ export default function About() {
     <div className="font-base">
       <h1 className="text-2xl font-heading sm:text-4xl">About me</h1>
       <div className="mt-8 text-base sm:text-lg">
-      <h2 className="text-2xl font-heading sm:text-2xl">Vibe</h2>
 
-        <p>
-          I like to be outside in the sun and in the water. I'm happiest
-          when I'm doing one of those two every day. I like to move around and I tend to
-          be really into a physical hobby. Which one varies; in the past it's been hiking,
-          sport climbing, running, or my current top choice, surfing. I also love to bike, but
-          in more of a "going around town" kinda way than a "spandex outfit" kinda way. 
-        </p>
-
-        <br/>
       <h2 className="text-2xl font-heading sm:text-2xl">Work</h2>
         <p>
           For work, I do machine learning development and research. I've worked on a few different
@@ -31,9 +21,22 @@ export default function About() {
         </p>
 
         <br/>
+
+      <h2 className="text-2xl font-heading sm:text-2xl">Life in general</h2>
+
+        <p>
+          I like to be outside in the sun and in the water. I'm happiest
+          when I'm doing one of those two every day. I like to move around and I tend to
+          be really into a physical hobby. Which one varies; in the past it's been hiking,
+          sport climbing, running, or my current top choice, surfing. I also love to bike, but
+          in more of a "going around town" kinda way than a "spandex outfit" kinda way. 
+        </p>
+
+        <br/>
+      
       <h2 className="text-2xl font-heading sm:text-2xl">Social stuff</h2>
         <p>
-          I'm happiest when I'm part of a big happy community. I like to make new friends
+          I'm happiest when I'm part of a big social community. I like to make new friends
           and especially enjoy bringing people together over a delicious meal. Once I land
           permanently in New York in July, my first order of business will be finding a big 
           dinner table. 
