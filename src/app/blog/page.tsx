98a7375bef5b32a6e22d256198753662fb9474ab
dhpitt <david.h.pitt@gmail.com>
2025-06-30 11:13:11 -0400

    update blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,7 +12,9 @@ export default async function Blog() {
 
   return (
     <div>
-      <h1 className="font-heading mb-8 text-2xl sm:text-4xl">David's thoughts</h1>
+      <h1 className="font-heading mb-8 text-2xl sm:text-4xl">David's blog</h1>
+      <p>Thoughts are my own, et cetera</p>
+      <br/>
       <div className="flex flex-col gap-5">
         {posts.map((post) => {
           return (
